4e7bdb0199e9eac3c5a30c57f160acdbdcd59bb3
Ampong Boadu <ampsboadu@gmail.com>
2023-04-21 21:49:53 +0000

    Header file edited to include print prototypes

--- sort.h
+++ sort.h
@@ -1,3 +1,6 @@
+#ifndef _SORT_H
+#define _SORT_H
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,3 +14,8 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif
